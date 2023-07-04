e9eca5fa008e819957da15dca2c79d289489d22b
Egor Subbotin <egorsubbotin1997@gmail.com>
2023-07-04 21:04:50 +0500

    Commit Homework1

--- homework-1/main.py
+++ homework-1/main.py
@@ -4,21 +4,70 @@ from utils import *
 from classes import Table
 import psycopg2
 
-employees_data = read_csv('north_data/employees_data.csv')
-employees_table = Table(employees_data)
-print(get_sql_command(employees_table))
+# employees_data = read_csv('north_data/employees_data.csv')
+# employees_table = Table(employees_data)
+# print(get_sql_command(employees_table))
+#
+# #Подключаемся к БД
+# conn = psycopg2.connect(host="localhost",
+#                         database="north",
+#                         user="postgres",
+#                         password="Subbotin1")
+# try:
+#     with conn:
+#         with conn.cursor() as cur:
+#             cur.execute(get_sql_command(employees_table))
+#             rows = cur.fetchall()
+#             for row in rows:
+#                 print(row)
+# finally:
+#     conn.close()
 
-#Подключаемся к БД
+# Подключаемся к БД
 conn = psycopg2.connect(host="localhost",
                         database="north",
                         user="postgres",
                         password="Subbotin1")
+
 try:
     with conn:
-        with conn.cursor() as cur:
-            cur.execute(get_sql_command(employees_table))
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
+        with conn.cursor() as cursor:
+            with open('north_data/customers_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+                        "INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+                        row
+                    )
+
+finally:
+    conn.close()
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            with open('north_data/employees_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+                        "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s, %s)",
+                        row
+                    )
+finally:
+    conn.close()
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            with open('north_data/orders_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+                        "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)",
+                        row
+                    )
 finally:
     conn.close()
--- homework-1/utils.py
+++ homework-1/utils.py
@@ -5,17 +5,22 @@ from classes import Table
 def read_csv(path_to_csv):
     with open(path_to_csv, newline='', encoding='utf-8') as f:
         data = list(csv.reader(f, quotechar='"'))
-        columns_name = data[0]
+        columns_name = tuple(data[0])
+
+        for column_name in columns_name:
+            column_name = column_name.replace('"', '')
+        columns_name.append(column_name)
+
         data_for_columns = []
         for row in data[1::]:
             data_for_columns.append(tuple(row))
         table_name = path_to_csv.split("/")[1].split("_")[0]
 
-        return tuple(columns_name), data_for_columns, table_name
+        return columns_name, data_for_columns, table_name
 
 
 def get_sql_command(object_table: Table):
-    return f"INSERT INTO {object_table.name} VALUES {object_table.columns}, {tuple(object_table.data)}"
+    return f"INSERT INTO {object_table.name} {object_table.columns} VALUES {tuple(object_table.data)}"
 
 
 print(read_csv('north_data/employees_data.csv'))
